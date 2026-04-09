6c02a4c24de55b103b31aa12aa13af05d00f6c0b
heitor-diniz <htrdnz92@gmail.com>
2026-04-09 11:35:06 -0300

    separate zap send

--- js/main.js
+++ js/main.js
@@ -86,18 +86,6 @@ window.addEventListener('scroll', () => {
 });
 
 // ==============================
-// WhatsApp Send Function
-// ==============================
-function sendWhatsApp() {
-  const name = encodeURIComponent(document.getElementById('name').value);
-  const email = encodeURIComponent(document.getElementById('email').value);
-  const message = encodeURIComponent(document.getElementById('message').value);
-  const whatsappNumber = "5531991400841"; 
-  const whatsappLink = `https://wa.me/${whatsappNumber}?text=Nome: ${name}%0AEmail: ${email}%0AMensagem: ${message}`;
-  window.open(whatsappLink,"_blank");
-}
-
-// ==============================
 // Redirect index.html to Browser Language
 // ==============================
 document.addEventListener('DOMContentLoaded', () => {
