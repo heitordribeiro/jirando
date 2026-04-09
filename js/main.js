74345e72053f72ba8dfbae06c52b2e26f35fd35d
heitor-diniz <htrdnz92@gmail.com>
2026-04-09 10:07:06 -0300

    remove footer info from css

--- js/main.js
+++ js/main.js
@@ -64,41 +64,6 @@ document.addEventListener('click', function (e) {
   }
 });
 
-/*
-
-// ==============================
-// Contact Form Submission
-// ==============================
-const contactForm = document.getElementById('contactForm');
-
-// Initialize EmailJS with your user ID
-emailjs.init('cF-aWnWqc8T5iFeZ2');
-
-if(contactForm) {
-    contactForm.addEventListener('submit', function(e){
-        e.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('message').value;
-
-        emailjs.send('service_d4s4v4q', 'template_quek7dx', {
-            from_name: name,
-            from_email: email,
-            message: message,
-            to_email: 'contactfromsite@jirando.com'
-        }).then(() => {
-            alert('Email enviado com sucesso!');
-        }).catch(err => {
-            // Log the error to the console to inspect it
-            console.error('Error sending email:', err);
-
-            // Display the error message or detailed error properties
-            alert('Erro ao enviar email: ' + (err.message || err));
-        });
-    });
-}
-
-*/
 
 // ==============================
 // WhatsApp Icon Float
