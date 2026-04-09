bf6df0dcbde0f266baca8bb02ff6bd5b017d08c5
heitor-diniz <htrdnz92@gmail.com>
2026-04-09 12:07:21 -0300

    test

---
+++ -emailjs.js
@@ -0,0 +1,19 @@
+emailjs.init({
+  publicKey: 'YOUR_PUBLIC_KEY',
+  // Do not allow headless browsers
+  blockHeadless: true,
+  blockList: {
+    // Block the suspended emails
+    list: ['foo@emailjs.com', 'bar@emailjs.com'],
+    // The variable contains the email address
+    watchVariable: 'userEmail',
+  },
+  limitRate: {
+    // Set the limit rate for the application
+    id: 'app',
+    // Allow 1 request per 10s
+    throttle: 10000,
+  },
+});
+
+emailjs.send(serviceID, templateID, templateParams, options);
