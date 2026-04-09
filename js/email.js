42c772d6c83285a07c8a0fca1888be4502af80b4
heitor-diniz <htrdnz92@gmail.com>
2026-04-09 12:02:29 -0300

    new funcion email

--- js/email.js
+++ js/email.js
@@ -1,31 +1,25 @@
 // ==============================
 // Contact Form Submission
 // ==============================
-const contactForm = document.getElementById('contactForm');
 
-// Initialize EmailJS with your user ID
-emailjs.init('cF-aWnWqc8T5iFeZ2');
 
-if(contactForm) {
-    contactForm.addEventListener('submit', function(e){
-        e.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('message').value;
+  // Initialize EmailJS with your user ID
+    (function(){
+      emailjs.init("cF-aWnWqc8T5iFeZ2"); // Replace with your EmailJS user ID
+    })();
 
-        emailjs.send('service_d4s4v4q', 'template_quek7dx', {
-            from_name: name,
-            from_email: email,
-            message: message,
-            to_email: 'contactfromsite@jirando.com'
-        }).then(() => {
-            alert('Email enviado com sucesso!');
-        }).catch(err => {
-            // Log the error to the console to inspect it
-            console.error('Error sending email:', err);
+    function sendEmail(event) {
+      event.preventDefault(); // Prevent default form submission
 
-            // Display the error message or detailed error properties
-            alert('Erro ao enviar email: ' + (err.message || err));
+      //const form = document.getElementById('contact-form');
+      const form = document.getElementById('contactForm');
+
+      emailjs.sendForm('service_d4s4v4q', 'template_quek7dx', form)
+        .then(() => {
+          alert('Email sent successfully!');
+          form.reset();
+        }, (error) => {
+          console.error('Failed to send email:', error);
+          alert('Failed to send email. Check console for details.');
         });
-    });
-}
+    }
