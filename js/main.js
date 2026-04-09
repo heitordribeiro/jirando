9d8d7bc8bef1047403d1e80bb1a5756cc8b58efb
heitor-diniz <htrdnz92@gmail.com>
2026-04-08 23:12:42 -0300

    email on main js

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ function clearStorage() {
   sessionStorage.clear();
 }
 
+
+
 // ==============================
 // Show Footer Only on Desktop
 // ==============================
@@ -62,6 +64,37 @@ document.addEventListener('click', function (e) {
   }
 });
 
+// ==============================
+// Contact Form Submission
+// ==============================
+const contactForm = document.getElementById('contactForm');
+
+// Initialize EmailJS with your user ID
+emailjs.init('cF-aWnWqc8T5iFeZ2');
+
+if(contactForm) {
+    contactForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        emailjs.send('service_d4s4v4q', 'template_quek7dx', {
+            from_name: name,
+            from_email: email,
+            message: message,
+            to_email: 'contactfromsite@jirando.com'
+        }).then(() => {
+            alert('Email enviado com sucesso!');
+        }).catch(err => {
+            // Log the error to the console to inspect it
+            console.error('Error sending email:', err);
+
+            // Display the error message or detailed error properties
+            alert('Erro ao enviar email: ' + (err.message || err));
+        });
+    });
+}
 
 // ==============================
 // WhatsApp Icon Float
