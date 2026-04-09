29974f22218ed737aefd138e59d9ace68b07d06f
heitor-diniz <htrdnz92@gmail.com>
2026-04-09 12:33:51 -0300

    adjust email send en pt es

--- js/email.js
+++ js/email.js
@@ -1,25 +1,22 @@
-// ==============================
-// Contact Form Submission
-// ==============================
+// Initialize EmailJS
+(function(){
+  emailjs.init("cF-aWnWqc8T5iFeZ2"); // Replace with your EmailJS user ID
+})();
 
+// Get the form element
+const form = document.getElementById('contactForm');
 
-  // Initialize EmailJS with your user ID
-    (function(){
-      emailjs.init("cF-aWnWqc8T5iFeZ2"); // Replace with your EmailJS user ID
-    })();
+// Attach submit event listener
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent page refresh
 
-    function sendEmail(event) {
-      event.preventDefault(); // Prevent default form submission
-
-      //const form = document.getElementById('contact-form');
-      const form = document.getElementById('contactForm');
-
-      emailjs.sendForm('service_d4s4v4q', 'template_quek7dx', form)
-        .then(() => {
-          alert('Email sent successfully!');
-          form.reset();
-        }, (error) => {
-          console.error('Failed to send email:', error);
-          alert('Failed to send email. Check console for details.');
-        });
-    }
+  emailjs.sendForm('service_d4s4v4q', 'template_quek7dx', form)
+    .then(() => {
+      alert('Email sent successfully!');
+      form.reset();
+    })
+    .catch((error) => {
+      console.error('Failed to send email:', error);
+      alert('Failed to send email. Check console for details.');
+    });
+});
